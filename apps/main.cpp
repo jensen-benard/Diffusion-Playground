63ca95cde204d93779cb3c8459ffe2c8d03f0eee
Jensen <jensen.benard@protonmail.com>
2025-09-22 16:05:02 +1200

    Add explicit 2D diffusion solver

--- apps/main.cpp
+++ apps/main.cpp
@@ -1,21 +1,27 @@
 #include "monte_carlo.h"
+#include "diffusion_explicit.h"
 #include "sdl_system.h"
 
 int main(int argc, char* args[]) {
+
+    WindowConfig windowConfig = {
+        .width = 1200,
+        .height = 800,
+        .framesPerSecond = 60,
+        .channelsPerPixel = 4,
+        .bitsPerChannel = 8,
+        .channelDepthPerPixel = 32,
+        .pixelMapPitch = 0,
+        .redMask = 0x00ff0000,
+        .greenMask = 0x0000ff00,
+        .blueMask = 0x000000ff,
+        .alphaMask = 0xff000000
+    };
+
+    windowConfig.pixelMapPitch = windowConfig.width * windowConfig.channelsPerPixel;
+
     MonteCarloConfig config = {
-        .windowConfig = {
-           .width = 1200,
-           .height = 800,
-           .framesPerSecond = 60,
-           .channelsPerPixel = 4,
-           .bitsPerChannel = 8,
-           .channelDepthPerPixel = 32,
-           .pixelMapPitch = 0,
-           .redMask = 0x00ff0000,
-           .greenMask = 0x0000ff00,
-           .blueMask = 0x000000ff,
-           .alphaMask = 0xff000000
-        },
+        .windowConfig = windowConfig,
         .originX = config.windowConfig.width / 2,
         .originY = config.windowConfig.height / 2,
         .totalParticles = 10000,
@@ -25,16 +31,28 @@ int main(int argc, char* args[]) {
         .recordMeanSquaredDisplacement = false
     };
 
-    config.windowConfig.pixelMapPitch = config.windowConfig.width * config.windowConfig.channelsPerPixel;
+    DiffusionExplicitConfig diffusionExplicitConfig = {
+        .windowConfig = windowConfig,
+        .diffusionConstant = 100.0f,
+        .gridSpacing = 4.0f,
+        .solverDeltaTime = 0.01f,
+        .simFramesPerSecond = 60,
+        .maxSteps = 100000,
+    };
+
+
     
 
     SDLSystem& sdlSystem = SDLSystem::getInstance();
 
-    MonteCarlo monteCarlo(config, sdlSystem);
+    //MonteCarlo monteCarlo(config, sdlSystem);
+
+    DiffusionExplicit diffusionExplicit(diffusionExplicitConfig, sdlSystem);
     
     while(!sdlSystem.hasQuit()) {
         sdlSystem.pollEvents();
-        monteCarlo.run();
+        //monteCarlo.run();
+        diffusionExplicit.run();
     }
     return 0;
 }
---
+++ include/diffusion_explicit.h
@@ -0,0 +1,54 @@
+#ifndef DIFFUSION_EXPLICIT_H
+#define DIFFUSION_EXPLICIT_H
+
+#include <vector>
+#include "window.h"
+#include "timer.h"
+
+struct DiffusionExplicitConfig {
+    // Window parameters
+    WindowConfig windowConfig;
+    
+    // Simulation parameters
+    float diffusionConstant;
+    float gridSpacing;
+    float solverDeltaTime;
+    int simFramesPerSecond;
+    int maxSteps;
+};
+
+
+class DiffusionExplicit {
+    public:
+        DiffusionExplicit(DiffusionExplicitConfig config, SDLSystem& sdlSystem);
+        ~DiffusionExplicit();
+
+        void run();
+
+        void stop();
+
+        bool hasStopped();
+
+
+    private:
+        DiffusionExplicitConfig config;
+        SDLSystem& sdlSystem;
+
+        EventCallbacks windowEventCallbacks {
+            .onClose = [this]() {this->stop();}
+        };
+
+        bool stopped = false;
+
+        std::vector<float> prevStepConcentrationGrid;
+
+        Window window;
+        
+        FixedTimer timer;
+
+        int getIndex(int x, int y);
+
+        int currentTimeStep;
+};
+
+#endif
